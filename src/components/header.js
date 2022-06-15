1eb439ae7aaa11038487475d500714c0ce781836
Jeff Tian <jeff.tian@outlook.com>
2022-06-15 18:08:01 +0800

    fix: menu items

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import SelectLanguage from './select-language'
 import {FormattedMessage} from 'react-intl'
 
 const Header = ({siteTitle, langs}) => (
-    <Menu inverted fluid widths={3}>
+    <Menu inverted fluid widths={4}>
         <Menu.Item as='a' href='https://github.com/Jeff-Tian/ass' target="_blank" name='源代码' active={false}><Icon
             name="github alternate"/></Menu.Item>
         <Menu.Item name={siteTitle} active={true} color={'red'}>
